ccf64338d2e95a353a2020f0d5de83d339fa4f02
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2023-06-17 09:07:15 -0500

    Add files via upload

---
+++ 0(n!).py
@@ -0,0 +1,4 @@
+# O(n!) = 0 of N factorial bascially means if you have 5! - to 5 * 4 * 3 * 2 * 1 
+# Permutation
+#Travelling Salesmen Problem
+#Not very important if anything the only thing I need to know is that it is very inneficient 
